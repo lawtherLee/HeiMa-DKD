4eef1a146bfd81aca5802bd6dff145783bbecf79
Lawther <823583958@qq.com>
2022-09-25 11:12:45 +0800

    feat:项目初始化2

--- src/icons/index.js
+++ src/icons/index.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import SvgIcon from '@/components/SvgIcon'// svg component
 
+// require.context 方法 可以实现批量导入文件
+// require.context(directory, useSubdirectories, regExp)
+// directory: 要查找的文件路径
+// useSubdirectories: 是否查找子目录
+// regExp: 要匹配文件的正则
+// 返回一个方法
+
+// 返回的方法具有一个 keys 属性
+// keys 函数 返回一个数组
+
 // register globally
 Vue.component('svg-icon', SvgIcon)
 
